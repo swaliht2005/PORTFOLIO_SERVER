f55b945cd83e347e60589ab8bc8b63a346963435
muhammed swalih.t <salumuhammadswalih@mil.com>
2025-12-19 22:40:23 +0530

    Fix: adjust project structure for Render deployment

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: ["https://portfolio-client-wtpn.vercel.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 app.use("/api/auth", 
     express.json({ limit: "50mb" }), 
