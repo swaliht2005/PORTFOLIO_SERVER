fbdbdc7107a2173dec5d834f52bdc5bd8f2a60b1
muhammed swalih.t <salumuhammadswalih@mil.com>
2025-12-25 21:05:13 +0530

    fix: update CORS for Vercel and switch to live API

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: ["https://portfolio-client-wtpn.vercel.app", "http://localhost:5173"],
+    origin: ["https://portfolio-client-wtpn.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
